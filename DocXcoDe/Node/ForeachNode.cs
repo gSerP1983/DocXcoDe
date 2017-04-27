2a27a8aac1954f8c3f249e1cacc39c84661d6678
Sergey Petrov <gserp1983@gmail.com>
2017-04-27 18:21:47 +0300

    add foreachNode

---
+++ DocXcoDe/Node/ForeachNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocXcoDe.Node
+{
+    public class ForeachNode : BaseQueryNode, IVisualNode
+    {
+        public override bool IsLeaf { get { return false; } }
+        public OpenXmlElement GetElement()
+        {
+            var run = new Run();
+            foreach (var dataRow in Data.Rows.Cast<DataRow>())
+            {
+                foreach (var node in Nodes.OfType<IVisualNode>())
+                    run.AppendChild(node.GetElement());
+            }
+            return new Paragraph(run);
+        }
+    }
+}
