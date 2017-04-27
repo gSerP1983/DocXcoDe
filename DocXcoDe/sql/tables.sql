b17556dc9e0ffe8509351f9257c1a7ce9065027e
Sergey Petrov <gserp1983@gmail.com>
2017-04-27 17:31:50 +0300

    tables.sql

---
+++ DocXcoDe/sql/tables.sql
@@ -0,0 +1,12 @@
+SELECT 
+	SCHEMA_NAME(o.schema_id) AS [Схема], 
+	o.name AS [Наименование], 
+	o.object_id AS _object_id, 
+	ep.value AS [Описание]
+FROM sys.objects o 
+	INNER JOIN sys.extended_properties ep 
+		ON ep.major_id = o.object_id 
+		AND ep.minor_id = 0 
+		AND ep.name = 'MS_Description' 
+WHERE o.type_desc = 'USER_TABLE'
+ORDER BY [Схема], [Наименование] 
