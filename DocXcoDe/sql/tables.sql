f52c223f3bde1c31e73c5c3d87e7b2a726e139e2
Dmatrix <dimasebakovgoo@gmail.com>
2018-09-14 15:18:11 +0300

    Exclude sysdiagrams from tables

--- DocXcoDe/sql/tables.sql
+++ DocXcoDe/sql/tables.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT
 	SCHEMA_NAME(o.schema_id) + '.' + o.name AS [Наименование], 
 	o.object_id AS _object_id, 
 	ep.value AS [Описание]
@@ -7,5 +7,7 @@ FROM sys.objects o
 		ON ep.major_id = o.object_id 
 		AND ep.minor_id = 0 
 		AND ep.name = 'MS_Description' 
-WHERE o.type_desc = 'USER_TABLE'
+WHERE 
+	o.type_desc = 'USER_TABLE'
+	AND o.name not like 'sys%'
 ORDER BY [Наименование] 
